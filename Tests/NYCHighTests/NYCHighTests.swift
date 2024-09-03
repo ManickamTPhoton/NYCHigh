b6c739303d3e47ea189fff66a29a8e0198d31fba
ManickamTPhoton <manickam.thankgaraj@adusa.com>
2024-09-03 17:00:58 +0530

    First commit

---
+++ Tests/NYCHighTests/NYCHighTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import NYCHigh
+
+final class NYCHighTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
