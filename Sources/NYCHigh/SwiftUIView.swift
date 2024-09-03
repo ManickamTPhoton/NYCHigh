3782523d7306cecf77312f86e6d2530350ced35f
ManickamTPhoton <manickam.thankgaraj@adusa.com>
2024-09-03 17:08:07 +0530

    Second commit

--- Sources/NYCHigh/SwiftUIView.swift
+++ Sources/NYCHigh/SwiftUIView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 public struct SwiftUIView: View {
+    
+    public init() {}
+    
     public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
